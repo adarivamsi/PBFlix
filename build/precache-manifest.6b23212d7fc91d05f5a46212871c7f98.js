aaf64825e89f47da5765b146a454392fecef5ac3
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-06-10 19:34:01 -0400

    Add files via upload

---
+++ build/precache-manifest.6b23212d7fc91d05f5a46212871c7f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04277440e5a3be88fdec4ffaea66531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c5898c3ec01d849a24",
+    "url": "/static/css/main.f68ae16b.chunk.css"
+  },
+  {
+    "revision": "6b69904ff3a51a07d580",
+    "url": "/static/js/2.dce47174.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.dce47174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c5898c3ec01d849a24",
+    "url": "/static/js/main.7fd83026.chunk.js"
+  },
+  {
+    "revision": "e92e7dbebd49ca6ee9cb",
+    "url": "/static/js/runtime-main.27921a7e.js"
+  }
+]);
