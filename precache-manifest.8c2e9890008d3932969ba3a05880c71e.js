1570dfd4ae16473b9f7126fd25b171387dc98748
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-06-27 18:10:47 -0400

    Updates

---
+++ precache-manifest.8c2e9890008d3932969ba3a05880c71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725d8c128b10f2eec49700cb91e370ad",
+    "url": "/adarivamsi.github.io/pbflix/index.html"
+  },
+  {
+    "revision": "97187e15a96296a86650",
+    "url": "/adarivamsi.github.io/pbflix/static/css/main.f34b491f.chunk.css"
+  },
+  {
+    "revision": "9c9238e325013bdb97a2",
+    "url": "/adarivamsi.github.io/pbflix/static/js/2.83b4b532.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/adarivamsi.github.io/pbflix/static/js/2.83b4b532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97187e15a96296a86650",
+    "url": "/adarivamsi.github.io/pbflix/static/js/main.9422e770.chunk.js"
+  },
+  {
+    "revision": "ce5fd5a9b45b19d90474",
+    "url": "/adarivamsi.github.io/pbflix/static/js/runtime-main.58ac4e61.js"
+  }
+]);
